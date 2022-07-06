27f1e9877d392dbd5c58580bfe771aea7dba8a84
Adrian Galindo <adrian17951@gmail.com>
2022-07-06 00:15:33 -0500

    Organize imports

--- src/main/java/mx/com/ids/ejercicio/Empleado/controller/airport/AirportController.java
+++ src/main/java/mx/com/ids/ejercicio/Empleado/controller/airport/AirportController.java
@@ -2,15 +2,12 @@ package mx.com.ids.ejercicio.Empleado.controller.airport;
 
 
 import mx.com.ids.ejercicio.Empleado.models.entity.airport.Airport;
-import mx.com.ids.ejercicio.Empleado.models.entity.employee.Employee;
 import mx.com.ids.ejercicio.Empleado.models.service.airport.IAirportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
